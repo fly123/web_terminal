0de95ee2977b10192b8085d59edc2e291eb188c6
fly123 <1196722167@qq.com>
2012-02-18 09:15:33 -0800

    first

---
+++ Makefile
@@ -0,0 +1,3 @@
+test:
+	python main.py 
+	#firefox http://127.0.0.1:8080
---
+++ static/js/postorder.js
@@ -0,0 +1,20 @@
+// JavaScript Document
+function postorder(type)
+  {
+      if ((type == "enter" && (event.keyCode==13)) || type == "click")
+      {
+	  var order = document.getElementById("order").value;
+      //clear order
+      document.getElementById("order").value = '';
+	  var ListNode = document.getElementById("response");
+	  var xmlHttp = new XMLHttpRequest();
+	  var url = "/receiveorder";
+	  xmlHttp.onreadystatechange=function() {
+	  	ListNode.innerHTML = xmlHttp.responseText;
+	  	return true;
+	  	}
+	  xmlHttp.open("POST",url,true);
+	  xmlHttp.setRequestHeader("Content-Type","application/x-www-form-urlencoded"); 
+	  xmlHttp.send("order="+order);
+      }
+  }
