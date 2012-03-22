bb6d396e8d453e06006e9436b9004b5115e6ba1e
yf <317012583@qq.com>
2012-03-22 22:00:00 +0800

    improve~~

--- static/js/gui.js
+++ static/js/gui.js
@@ -1,7 +1,7 @@
 var appnum=0;
 function open_app(app_div)
 {
-	
+
 	var path=$(app_div).attr('path');
 	var app_name=det_app(path)
 	switch (app_name)
@@ -10,10 +10,6 @@ function open_app(app_div)
 			notepad_creat();
 			break;}
 	}
-	/*var tmp="#a+"+tmp.id;
-	alert(tmp);
-	var path=$('#aapp1').attr('href')
-	alert(path);*/
 }
 function det_app(app_path)
 {
@@ -46,6 +42,7 @@ function notepad_close(notepad)
 	var sel='#'+notepad.id;
 	$(sel).hide('fast');
 }
+//未实现迭代前的实验函数
 function drag1(div)
 {
 	//$('div').not('#app1').css('z-index','-1');
@@ -56,20 +53,24 @@ function drag1(div)
 	});
 	
 }
+//使notepad可移动和可变化大小
 function pad_drag(div)
 {
 	//$('div').not('#notepad1').css('z-index','-1');
-	$(div).draggable({
-	containment: "#notepad_result", 
-	scroll: false 
-	});
-	$(div).resizable({
+	if(!is_draggable($(div)))
+	{
+		$(div).draggable({
+		containment: "#notepad_result", 
+		scroll: false 
+		});
+		$(div).resizable({
 				maxHeight: 700,
 				maxWidth: 1300,
 				minHeight: 250,
 				minWidth: 300,
 				alsoResize: ".notepad_body",
-			});
+		});
+	}
 }
 /*
 function ajax_request()
@@ -84,22 +85,33 @@ function ajax_request()
 	}
 	return false;
 }*/
+function is_draggable(div)
+{
+	var tmpdiv=div[0];
+	var classname=tmpdiv.className;
+	if(classname.indexOf('ui-draggable')>-1)
+	{
+		return 1;
+	}else 
+	{
+		return false;
+	}
+}
 $(document).ready(function(){
 	$('#app_board').load('/static/tpls/appbutton.html');
 	$('div.appbutton').draggable({
 	containment: "#app_board", 
 	scroll: false 
 	});
-	/*$('#test').click(
-		function()
-		{
-			$('div.notepad').resizable({
-				maxHeight: 700,
-				maxWidth: 1300,
-				minHeight: 250,
-				minWidth: 300,
-				alsoResize: ".notepad_body",
-			});
+	//后台迭代后开启此绑定函数
+	/*$('div.appbutton').bind('mouseover',function(){
+		if(!is_draggable($(this))){
+			$(this).draggable({
+				grid: [ 78, 91 ],
+				containment: "#app_board", 
+				scroll: false 
+				});
+			}
 		}
-	)*/
+	);*/
 });
